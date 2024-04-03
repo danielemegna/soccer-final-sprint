b48d46b918650c8e8e4314d39b04409f4d712a37
Daniele Megna <daniele.megna@qmates.tech>
2024-04-03 07:57:10 +0200

    Handle team matches
    rewritten championshipDay function !

--- src/final_sprint.ts
+++ src/final_sprint.ts
@@ -9,10 +9,23 @@ export type MatchDay = {
   date: Date
 }
 
-export function championshipDay(leaderboard: Leaderboard, topTeamCalendar: TeamCalendar): MatchDay {
-  const advantagePoints = leaderboard[0].points - leaderboard[1].points;
-  const advantageMatchesRaw = advantagePoints / 3;
-  const advantageMatches = Number.isInteger(advantageMatchesRaw) ?
-    advantageMatchesRaw - 1 : Math.floor(advantageMatchesRaw);
-  return topTeamCalendar[topTeamCalendar.length - advantageMatches - 1];
+export function championshipDay(leaderboard: Leaderboard, topTeamCalendar: TeamCalendar): MatchDay | null {
+  let topTeamPoints = leaderboard[0].points;
+  let { team: secondTeamName, points: secondTeamPoints } = leaderboard[1];
+
+  for (let matchIndex = 0; matchIndex < topTeamCalendar.length; matchIndex++) {
+    const matchDay = topTeamCalendar[matchIndex];
+
+    topTeamPoints += 3;
+    if (matchDay.opponent != secondTeamName)
+      secondTeamPoints += 3;
+
+    const advantagePoints = topTeamPoints - secondTeamPoints;
+    const remaningMatches = topTeamCalendar.length - (matchIndex + 1);
+    const remaningPoints = remaningMatches * 3;
+    if(remaningPoints < advantagePoints)
+      return matchDay;
+  }
+
+  return null;
 }
