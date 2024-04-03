f14378baf4d12ef58890d2341d865c9ba7b0a177
Daniele Megna <daniele.megna@qmates.tech>
2024-04-03 08:25:16 +0200

    Sort differently test cases

--- src/final_sprint.test.ts
+++ src/final_sprint.test.ts
@@ -3,6 +3,31 @@ import { Leaderboard, TeamCalendar, championshipDay } from './final_sprint';
 
 describe('championship day', () => {
 
+  test('with close top teams match [REAL 2024 SEASON AT 02 APRIL]', () => {
+    const leaderboard: Leaderboard = [
+      { team: 'inter', points: 79 },
+      { team: 'milan', points: 65 },
+      { team: 'juventus', points: 59 },
+      { team: 'bologna', points: 57 },
+    ]
+    const topTeamCalendar: TeamCalendar = [
+      { opponent: 'udinese', date: new Date('2024/04/08') },
+      { opponent: 'cagliari', date: new Date('2024/04/14') },
+      { opponent: 'milan', date: new Date('2024/04/22') },
+      { opponent: 'torino', date: new Date('2024/04/28') },
+      { opponent: 'sassuolo', date: new Date('2024/05/05') },
+      { opponent: 'frosinone', date: new Date('2024/05/12') },
+      { opponent: 'lazio', date: new Date('2024/05/19') },
+      { opponent: 'verona', date: new Date('2024/05/26') },
+    ]
+
+    const result = championshipDay(leaderboard, topTeamCalendar);
+
+    expect(result).toEqual(
+      { opponent: 'milan', date: new Date('2024/04/22') }
+    )
+  })
+
   test('without top teams match and odd advantage', () => {
     const leaderboard: Leaderboard = [
       { team: 'inter', points: 79 },
@@ -50,31 +75,6 @@ describe('championship day', () => {
 
     expect(result).toEqual(
       { opponent: 'sassuolo', date: new Date('2024/05/05') },
-    )
-  })
-
-  test('with close top teams match', () => {
-    const leaderboard: Leaderboard = [
-      { team: 'inter', points: 79 },
-      { team: 'milan', points: 65 },
-      { team: 'juventus', points: 59 },
-      { team: 'bologna', points: 57 },
-    ]
-    const topTeamCalendar: TeamCalendar = [
-      { opponent: 'udinese', date: new Date('2024/04/08') },
-      { opponent: 'cagliari', date: new Date('2024/04/14') },
-      { opponent: 'milan', date: new Date('2024/04/22') },
-      { opponent: 'torino', date: new Date('2024/04/28') },
-      { opponent: 'sassuolo', date: new Date('2024/05/05') },
-      { opponent: 'frosinone', date: new Date('2024/05/12') },
-      { opponent: 'lazio', date: new Date('2024/05/19') },
-      { opponent: 'verona', date: new Date('2024/05/26') },
-    ]
-
-    const result = championshipDay(leaderboard, topTeamCalendar);
-
-    expect(result).toEqual(
-      { opponent: 'milan', date: new Date('2024/04/22') }
     )
   })
 
